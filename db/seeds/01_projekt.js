7fa7610b8a198056a719256f2ecaddf39b5edbfc
Felix Lang <fe06.lang@googlemail.com>
2017-12-12 17:55:32 +0100

    project seeds

--- db/seeds/01_projekt.js
+++ db/seeds/01_projekt.js
@@ -10,7 +10,8 @@ exports.seed = function(knex, Promise) {
           text: " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam", 
           karma: 25,
           projecttype: "newProject",
-          author: 1
+          author: 1,
+          imagepath: "/images/Chrysatheum.jpg"
 
           
         },
@@ -19,10 +20,70 @@ exports.seed = function(knex, Promise) {
           text: " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam", 
           karma: 25,
           projecttype: "newProject",
-          author: 2
+          author: 2,
+          imagepath: "/images/Desert.jpg"
           
         },
-        
+        {projectid: 3, 
+          name: 'Schnitzelwerkstatt',
+          text: " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam", 
+          karma: 25,
+          projecttype: "newProject",
+          author: 2,
+          imagepath: "/images/Hydrangeas.jpg"
+          
+        },
+        {
+          projectid: 4, 
+          name: 'Freudenhaus',
+          text: " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam", 
+          karma: 25,
+          projecttype: "newProject",
+          author: 2,
+          imagepath: "/images/Jellyfish.jpg"
+          
+        },
+        {
+          projectid: 5, 
+          name: 'Wiesenbesuch',
+          text: " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam", 
+          karma: 25,
+          projecttype: "newProject",
+          author: 2,
+          imagepath:"/images/Koala.jpg"
+          
+        },
+        {
+          projectid: 6, 
+          name: 'Keine Schule heute',
+          text: " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam", 
+          karma: 25,
+          projecttype: "newProject",
+          author: 2,
+          imagepath: "/images/Lighthouse.jpg"
+          
+        },
+        {
+          projectid: 7, 
+          name: 'Alex sein Projekt',
+          text: " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam", 
+          karma: 25,
+          projecttype: "newProject",
+          author: 2,
+          imagepath: "/images/Penguins.jpg"
+          
+        },
+        {
+          projectid: 8, 
+          name: 'Alex sein Projekt 2',
+          text: " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam", 
+          karma: 25,
+          projecttype: "newProject",
+          author: 2,
+          imagepath: "/images/Tulips.jpg"
+          
+        },
+
       ]);
     });
 };
