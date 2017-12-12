a7580dc477f1d08c975096b9f1d343ad3f3f18be
KevinKleine1 <kevin_kleine@web.de>
2017-12-12 16:26:53 +0100

    Another fix

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(function(req, res, next) {
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(express.static('/site/wwwroot'));
+app.use(express.static('images'));
 app.use('/', welcome);
 app.use(fileUpload());
 
