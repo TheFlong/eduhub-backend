0a03396bc011ae96f923152bff2a8b457a036f85
Felix Lang <fe06.lang@googlemail.com>
2017-12-15 15:19:38 +0100

    Member werden erstellt das Projekt bemember

--- controller/useraddsprojectctrl.js
+++ controller/useraddsprojectctrl.js
@@ -11,7 +11,10 @@ module.exports = {
     beFavorite2,
     cancelFavorite1,
     cancelFavorit2,
-    amIFavorite
+    amIFavorite,
+
+    beMemberEvent1,
+    beMemberEvent2
 
 };
 
@@ -40,6 +43,60 @@ function beMember2(req,res){
     })
 
 }
+function beMemberEvent1(req,res,next){
+    knex.select('surname as temp1', 'forename as temp2').from('User').where('userid', req.body.uhp_iduser)
+        .then(function(response, response1){
+            response : response[0].temp1;
+            req.name = response[0];
+           /*  console.log(req.name.temp1); */
+        })   
+       .then(function(){
+            knex.select('project_name as temp3').from('Project').where('projectid',req.body.uhp_idproject)
+           .then(function(response1){
+            response1 : response1[0].temp3; 
+            req.projectname = response1[0];  
+            next();
+            /* console.log(req.projectname.temp3); 
+            console.log(req.name.temp1);    */
+        }) 
+    })  
+}
+function beMemberEvent2(req,res, next){
+    var forename = req.name.temp2;
+    var surname = req.name.temp1;
+    var projectname= req.projectname.temp3
+    console.log(forename + surname + projectname)
+    
+    return knex.transaction(function(t){
+        return knex('Project')
+        .transacting(t)
+        .insert({
+            project_name: req.name.temp2 + ' ' + req.name.temp1 + ' ' + 'ist dem Projekt' + ' "'+ req.projectname.temp3 + '" ' + 'beigetreten!',
+            project_text: "test234",
+            project_karma: 0,
+            project_projecttype: "bemember",
+            project_author: req.body.uhp_iduser,
+            project_membercount: 1
+        })
+        .then(function(response){
+            return knex('UserHasProject')
+            .transacting(t)
+            .insert({
+                uhp_iduser: req.body.uhp_iduser,
+                uhp_idproject: response[0],
+                uhp_userrole: "author"
+
+            })
+        })
+        .then(t.commit)
+        .catch(t.rollback)
+        .then(function(){
+            next();
+        })
+
+    });
+}
+
 function cancelMembership1(req,res,next){
     knex('Project')
     .where('projectid', req.body.uhp_idproject)
--- server.js
+++ server.js
@@ -101,8 +101,8 @@ app.post('/resource', resourcectrl.addOne)
 
 
 //UseraddsProjectCtrl
-app.use('/useraddsproject/beMember', useraddsprojectctrl.beMember1);
-app.post('/useraddsproject/beMember', useraddsprojectctrl.beMember2);
+app.use('/useraddsproject/beMember',useraddsprojectctrl.beMember1,  useraddsprojectctrl.beMemberEvent1);
+app.post('/useraddsproject/beMember', useraddsprojectctrl.beMemberEvent2, useraddsprojectctrl.beMember2);
 //req.body.uhp_iduser, req.body.uhp_idproject
 app.use('/useraddsproject/cancelMembership', useraddsprojectctrl.cancelMembership1);
 app.delete('/useraddsproject/cancelMembership', useraddsprojectctrl.cancelMembership2);
