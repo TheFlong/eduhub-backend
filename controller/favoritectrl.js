fdf3500008e40b4c599acc5c65139c09c284fdad
Felix Lang <fe06.lang@googlemail.com>
2017-12-15 16:49:33 +0100

    favorite event

--- controller/favoritectrl.js
+++ controller/favoritectrl.js
@@ -1,107 +1,34 @@
 var knex = require('../db/knex')
-
 module.exports = {
-    beMember1,
-    beMember2,
-    beMemberEvent1,
-    beMemberEvent2,
-    cancelMembership1,
-    cancelMembership2,
-    cancelMembershipEvent1,
-    cancelMembershipEvent2,
-    amIMember
-};
+    beFavorite1,
+    beFavorite2,
+    beFavoriteEvent1,
+    beFavoriteEvent2,
+    cancelFavorite1,
+    cancelFavorit2,
+    cancelFavoriteEvent1,
+    cancelFavoriteEvent2,
+    amIFavorite
+}
 
-function beMember1 (req,res,next){
-knex('UserHasProject')
-        .insert({
-            uhp_iduser: req.body.uhp_iduser,
-            uhp_idproject: req.body.uhp_idproject,
-            uhp_userrole: "member"
-        }).then(function(){
-             req.projectid = req.body.uhp_idproject;
-             next();
-        })
-}
-function beMember2(req,res){
-    knex('Project')
-    .where('projectid', req.projectid)
-    .increment('project_membercount', 1) 
-    .then(function(response1) {
-        knex.select()
-        .from('Project')
-        .where('projectid', req.projectid)
-        .then(function(Project){
-            res.send(Project);
-        })
-    })
 
-}
-function beMemberEvent1(req,res,next){
-    knex.select('surname as temp1', 'forename as temp2').from('User').where('userid', req.body.uhp_iduser)
-        .then(function(response, response1){
-            response : response[0].temp1;
-            req.name = response[0];
-        })   
-       .then(function(){
-            knex.select('project_name as temp3').from('Project').where('projectid',req.body.uhp_idproject)
-           .then(function(response1){
-            response1 : response1[0].temp3; 
-            req.projectname = response1[0];  
-            next();
-        }) 
-    })  
-}
-function beMemberEvent2(req,res, next){
-    return knex.transaction(function(t){
-        return knex('Project')
-        .transacting(t)
-        .insert({
-            project_name: req.name.temp2 + ' ' + req.name.temp1 + ' ' + 'ist dem Projekt' + ' "'+ req.projectname.temp3 + '" ' + 'beigetreten!',
-            project_text: "test234",
-            project_karma: 0,
-            project_projecttype: "bemember",
-            project_author: req.body.uhp_iduser,
-            project_membercount: 1,
-            Project_projectid: req.body.uhp_idproject
-        })
-        .then(function(response){
-            return knex('UserHasProject')
-            .transacting(t)
+
+function beFavorite1 (req,res,next){
+    knex('UserHasProject')
             .insert({
                 uhp_iduser: req.body.uhp_iduser,
-                uhp_idproject: response[0],
-                uhp_userrole: "author"
-
+                uhp_idproject: req.body.uhp_idproject,
+                uhp_userrole: "favorite"
+            }).then(function(){
+                 req.projectid = req.body.uhp_idproject;
+                 next();
             })
-        })
-        .then(t.commit)
-        .catch(t.rollback)
-        .then(function(){
-            next();
-        })
-
-    });
-}
-
-function cancelMembership1(req,res,next){
+ }
+function beFavorite2(req,res){
     knex('Project')
-    .where('projectid', req.body.uhp_idproject)
-    .decrement('project_membercount', 1)
-    .then(function(){
-        req.projectid = req.body.uhp_idproject;
-        req.userid = req.body.uhp_iduser;
-        next();
-   })
-
-}
-function cancelMembership2(req,res){
-    knex('UserHasProject')
-        .where('uhp_idproject',  req.projectid)
-        .andWhere('uhp_iduser', req.userid )
-        .andWhere('uhp_userrole', 'member')
-        .del()
-        .then(function(response1) { 
+        .where('projectid', req.projectid)
+        .increment('project_favcount', 1) 
+        .then(function(response1) {
             knex.select()
             .from('Project')
             .where('projectid', req.projectid)
@@ -109,8 +36,9 @@ function cancelMembership2(req,res){
                 res.send(Project);
             })
         })
+    
 }
-function cancelMembershipEvent1(req,res,next){
+function beFavoriteEvent1(req,res,next){
     knex.select('surname as temp1', 'forename as temp2').from('User').where('userid', req.body.uhp_iduser)
         .then(function(response, response1){
             response : response[0].temp1;
@@ -125,15 +53,15 @@ function cancelMembershipEvent1(req,res,next){
         }) 
     })  
 }
-function cancelMembershipEvent2(req,res, next){
+function beFavoriteEvent2(req,res, next){
     return knex.transaction(function(t){
         return knex('Project')
         .transacting(t)
         .insert({
-            project_name: req.name.temp2 + ' ' + req.name.temp1 + ' ' + 'hat das Projekt' + ' "'+ req.projectname.temp3 + '" ' + 'verlassen!',
-            project_text: "test234",
+            project_name: req.name.temp2 + ' ' + req.name.temp1 + ' ' + 'hat das Projekt' + ' "'+ req.projectname.temp3 + '" ' + 'als Favorit markiert!',
+            project_text: "test2345",
             project_karma: 0,
-            project_projecttype: "cancelmembership",
+            project_projecttype: "befavorite",
             project_author: req.body.uhp_iduser,
             project_membercount: 1,
             Project_projectid: req.body.uhp_idproject
@@ -156,13 +84,85 @@ function cancelMembershipEvent2(req,res, next){
 
     });
 }
-function amIMember(req, res){
+function cancelFavorite1(req,res,next){
+    knex('Project')
+    .where('projectid', req.body.uhp_idproject)
+    .decrement('project_favcount', 1)
+    .then(function(){
+        req.projectid = req.body.uhp_idproject;
+        req.userid = req.body.uhp_iduser;
+        next();
+   })
+
+}
+function cancelFavorit2(req,res){
     knex('UserHasProject')
-         .count('uhp_idproject as CNT')
+        .where('uhp_idproject',  req.projectid)
+        .andWhere('uhp_iduser', req.userid )
+        .andWhere('uhp_userrole', 'favorite')
+        .del()
+        .then(function(response1) { 
+            knex.select()
+                .from('Project')
+                .where('projectid', req.projectid)
+                .then(function(Project){
+                    res.send(Project);
+                })
+        })
+}
+function cancelFavoriteEvent1(req,res,next){
+    knex.select('surname as temp1', 'forename as temp2').from('User').where('userid', req.body.uhp_iduser)
+        .then(function(response, response1){
+            response : response[0].temp1;
+            req.name = response[0];
+        })   
+       .then(function(){
+            knex.select('project_name as temp3').from('Project').where('projectid',req.body.uhp_idproject)
+           .then(function(response1){
+            response1 : response1[0].temp3; 
+            req.projectname = response1[0];  
+            next();
+        }) 
+    })  
+}
+function cancelFavoriteEvent2(req,res, next){
+    return knex.transaction(function(t){
+        return knex('Project')
+        .transacting(t)
+        .insert({
+            project_name: req.name.temp2 + ' ' + req.name.temp1 + ' ' + 'hat das Projekt' + ' "'+ req.projectname.temp3 + '" ' + 'verlassen!',
+            project_text: "test234",
+            project_karma: 0,
+            project_projecttype: "cancelfavorite",
+            project_author: req.body.uhp_iduser,
+            project_membercount: 1,
+            Project_projectid: req.body.uhp_idproject
+        })
+        .then(function(response){
+            return knex('UserHasProject')
+            .transacting(t)
+            .insert({
+                uhp_iduser: req.body.uhp_iduser,
+                uhp_idproject: response[0],
+                uhp_userrole: "author"
+
+            })
+        })
+        .then(t.commit)
+        .catch(t.rollback)
+        .then(function(){
+            next();
+        })
+
+    });
+}
+function amIFavorite(req, res){
+    knex('UserHasProject')
+        .count('uhp_idproject as CNT')
         .where(function (){ 
         this.where('uhp_idproject', req.params.uhp_idproject)
                 .andWhere('uhp_iduser', req.params.uhp_iduser)
-                .andWhere('uhp_userrole', 'member')
+                .andWhere('uhp_userrole', 'favorite')
         })
         .then(function(response){
             res.send({
@@ -170,4 +170,3 @@ function amIMember(req, res){
             });
         })
 }
-
