4c9f2a43cbdc5180043fb38466363ade26207053
Felix Lang <fe06.lang@googlemail.com>
2017-12-19 15:42:27 +0100

    rechtschreibung

--- server.js
+++ server.js
@@ -59,8 +59,8 @@ app.put('/user', userctrl.changeOne);
 /* req.body.forename, req.body.surname, req.body.email,
 req.body.street, req.body.city, req.body.number, req.body.postcode, req.body.schoolid,
 req.body.function, subject1: req.body.subject1, subject2: req.body.subject2, subject3: req.body.subject3 */
-app.use('/user/changePicture',userctrl.changePicture1);
-app.put('/user/changePicture', userctrl.changePicture2);
+app.use('/user/changepicture',userctrl.changePicture1);
+app.put('/user/changepicture', userctrl.changePicture2);
 
 
 app.delete('/user',userctrl.deleteOne);
