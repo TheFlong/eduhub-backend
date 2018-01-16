7ef2e5af31559e2ed53d9d5c5f920869c8d86d22
Felix Lang <fe06.lang@googlemail.com>
2018-01-16 19:11:25 +0100

    Löschen von unnötigem Code

--- controller/tagctrl.js
+++ controller/tagctrl.js
@@ -7,34 +7,7 @@ module.exports = {
     getProjectsTags
 }
 
-/* Tag :  tagid , name
-ProjectHasTag: projecthastagid, idtag, idproject */
 
-function addOne(req, res){
-    return knex.transaction(function(t){
-        return knex('Tag')
-        .transacting(t)
-        .insert({
-            tag_name: req.body.tag_name,
-        })
-        .then(function(response){
-            return knex('ProjectHasTag')
-            .transacting(t)
-            .insert({
-                pht_idproject: req.body.pht_idproject,
-                pht_idtag: response[0]
-            })
-        })
-        .then(t.commit)
-        .catch(t.rollback)
-        .then(function() {
-            knex.select().from('ProjectHasTag')
-            .then(function(ProjectHasTag) {
-                res.send(ProjectHasTag);
-            })
-        }) 
-    });
-}
 function getProjectsTags(req,res){
     knex
         .select('tagid', 'tag_name').from('Tag')
