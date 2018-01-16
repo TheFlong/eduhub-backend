104fd5fcebd03a0cba323154d4387a8aba3c9a44
Felix Lang <fe06.lang@googlemail.com>
2018-01-16 17:53:54 +0100

    Nur Projekte anzeigen, keine Events

--- controller/searchctrl.js
+++ controller/searchctrl.js
@@ -8,6 +8,8 @@ function searchString(req,res){
     knex('Project')
     .select()
     .where('project_name', 'like', '%' + req.params.project_name + '%')
+    .andWhere('project_projecttype', "newProject")
     .orderBy('project_name', 'asc')
     .then(Project => res.send(Project));
+
 }
