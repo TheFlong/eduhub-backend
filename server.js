c85f88fa0c29e375b07a15e66afd168836f1b846
Felix Lang <fe06.lang@googlemail.com>
2018-01-19 11:34:21 +0100

    Kommentare

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+
 var express = require('express');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 const multer = require('multer');
 const fileUpload = require('express-fileupload');
 
+//Import der Controler Klassen
 var userctrl = require('./controller/userctrl');
 var projectctrl = require('./controller/projectctrl');
 var tagctrl = require('./controller/tagctrl');
@@ -51,59 +53,84 @@ const server = http.createServer(app);
 //userctrl Routes
 app.get('/user', userctrl.getAll);
 app.get('/user/:email', userctrl.getOne);
+//Input: email
 app.get('/user/getmyproject/:userid', userctrl.getMyProject);
+//Input: userid
 app.get('/user/getmyproject/favs/:userid',userctrl.getMyFavProjects);
+//Input: userid
 app.get('/user/profil/timeline/:userid', userctrl.getMyTimeline);
+////Input: userid
 
 app.use('/user/myevents/:userid', userctrl.getMyMeetings, userctrl.getMyMeetings2);
 app.get('/user/myevents/:userid', userctrl.getMyMeetings3);
+//Input: userid
 
 
 app.post('/user', userctrl.addOne);
-/* //req.files.foo, req.body.fileName, req.body.forename, req.body.surname, req.body.email,
-req.body.street, req.body.city, req.body.number, req.body.postcode, req.body.schoolid,
-req.body.function, subject1: req.body.subject1, subject2: req.body.subject2, subject3: req.body.subject3  */
+//Input:
+//req.files.foo, req.body.fileName, req.body.forename, req.body.surname, req.body.email,
+//req.body.street, req.body.city, req.body.number, req.body.postcode, req.body.schoolid,
+//req.body.function, subject1: req.body.subject1, subject2: req.body.subject2, subject3: req.body.subject3  
 
 app.put('/user', userctrl.changeOne);
-/* req.body.forename, req.body.surname, req.body.email,
-req.body.street, req.body.city, req.body.number, req.body.postcode, req.body.schoolid,
-req.body.function, subject1: req.body.subject1, subject2: req.body.subject2, subject3: req.body.subject3 */
+//Input:
+//req.body.forename, req.body.surname, req.body.email,
+//req.body.street, req.body.city, req.body.number, req.body.postcode, req.body.schoolid,
+//req.body.function, subject1: req.body.subject1, subject2: req.body.subject2, subject3: req.body.subject3
 app.use('/user/changepicture',userctrl.changePicture1);
 app.put('/user/changepicture', userctrl.changePicture2);
+//Input: req.body.userid, req.file.foo
 
 
 app.delete('/user',userctrl.deleteOne);
-// funktioniert noch nicht !!!!/////////////////////////////////
+//Input: req.body.email
 
 
 
 //projectctrl Routes
 app.get('/project/members/:projectid', projectctrl.getMembers);
+//Input: projectid
 app.get('/project', projectctrl.getAll);
+
 app.get('/project/:projectid', projectctrl.getOne);
+//Input: projectid
 app.get('/project/getReactions/:projectid',  projectctrl.getReactions);
+//Input: projectid
 app.get('/project/landingpage/get', projectctrl.getLandingPage);
+
 app.get('/project/documents/get/:projectid', projectctrl.getDocumentList);
+//Input: projectid
 app.get('/project/images/get/:projectid', projectctrl.getImageList);
+//Input: projectid
 
 //Filterfunktionen
 app.get('/project/filter/documentsimage/:projectid', filterctrl.getUploadEvents);
 app.get('/project/filter/memberevents/:projectid', filterctrl.getMemberEvents);
 app.get('/project/filter/comments/:projectid', filterctrl.getCommentEvents);
 app.get('/project/filter/termin/:projectid', filterctrl.getTerminEvents);
-
+//Input: projectid
 
 app.post('/project/addComment', reactionctrl.addComment);
-//req.body.Project_projectid(aktuelle Projektid),req.body.project_name(Titel für Reaktion),req.body.project_text(KommentarText),req.body.project_author(Nutzer id)
+//Input:
+//req.body.Project_projectid(aktuelle Projektid),req.body.project_name(Titel für Reaktion),req.body.project_text(KommentarText),
+//req.body.project_author(Nutzer id)
 app.post('/project/addImage', reactionctrl.addImage);
-/* //req.body.Project_projectid(aktuelle Projektid),req.body.project_name(Titel für Reaktion),req.body.project_text(Beschreibung),
-req.body.project_author(Nutzer id),req.body.fileName,req.files.foo */
+//Input:
+//req.body.Project_projectid(aktuelle Projektid),req.body.project_name(Titel für Reaktion),req.body.project_text(Beschreibung),
+//req.body.project_author(Nutzer id),req.body.fileName,req.files.foo
 app.post('/project/addDocument', reactionctrl.addDocument);
+//Input: req.files.foo, req.body.fileName, req.body.Project_projectid, req.body.project_name, req.body.project_text, req.body.project_author 
 app.post('/project/addTermin', reactionctrl.addTermin);
-
+//Input: req.files.foo, req.body.fileName, req.body.Project_projectid, req.body.project_name, req.body.project_text, req.body.project_author 
 app.use('/project/update', projectctrl.changeProject1);
 app.put('/project/update', projectctrl.changeProject2);
-
+//Input: req.body.changeid, req.body.projectid, req.body.userid, req.body.projectid
+// changeid [1]: Titel ändern,  req.body.newtitle
+// changeid [2]: Beschreibung ändern, req.body.newdescription
+// changeid [3]: Bild ändern,   req.files.foo, req.body.fileName,
+// changeid [4]: Projektbeginn ändern, req.body.newtermin
+// changeid [5]: Projektzielsetzung ändern, req.body.newgoal
+// changeid [6]: Schreibrechte ändern, req.body.newwriteRights
 app.delete('/project/delete', projectctrl.deleteProject)
 
 
@@ -174,6 +201,10 @@ app.get('/useraddsproject/karma/amILiker/:uhp_idproject/:uhp_iduser', karmactrl.
 
 
 
+
+var testctrl = require('./controller/zztest')
+
+app.get('/test', testctrl.test)
 
 
 app.listen(port, function(){
