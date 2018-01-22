75eabd6c42f87309ae697f72e6e3bc03394721b6
Felix Lang <fe06.lang@googlemail.com>
2018-01-22 22:27:26 +0100

    suche erweitert auf Ressource

--- controller/searchctrl.js
+++ controller/searchctrl.js
@@ -17,5 +17,13 @@ function searchString(req,res){
             .where('project_projecttype', "newProject")
             .andWhere('tag_name', 'like', '%' + req.params.project_name + '%')
     })
+    .union (function(){
+        this.select('projectid','project_name','project_text','project_karma','project_projecttype','project_author','project_activity','project_statement','project_imagepath','project_writeRights','project_membercount','project_favcount','project_termin','project_change','Project_projectid','project_created_at','project_updated_at')
+            .from('Project')
+            .join('ProjectHasResource', 'phr_idproject', 'projectid')
+            .join('Resource', 'resourceid', 'phr_idresource')
+            .where('project_projecttype', "newProject")
+            .andWhere('resource_name', 'like', '%' + req.params.project_name + '%')
+    })
     .then( User => res.send(User))
 }
