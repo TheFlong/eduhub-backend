e6d3d123af1e4a70cc9b66d7769c768cb65e84e1
Felix Lang <fe06.lang@googlemail.com>
2018-01-23 12:02:30 +0100

    suche fix

--- controller/searchctrl.js
+++ controller/searchctrl.js
@@ -5,7 +5,7 @@ module.exports = {
 }
 //Suche nach Teilstring in Tags und Projektnamen
 function searchString(req,res){
-    if(req.body.project_statement == null){
+    if(req.body.project_activity == null){
         knex.select('projectid','project_name','project_text','project_karma','project_projecttype','project_author','project_activity','project_statement','project_imagepath','project_writeRights','project_membercount','project_favcount','project_termin','project_change','Project_projectid','project_created_at','project_updated_at')
         .from('Project')
         .where('project_projecttype', "newProject")
@@ -28,11 +28,11 @@ function searchString(req,res){
         })
         .then( User => res.send(User))
     }
-    if(req.body.project_statement != null){
+    if(req.body.project_activity != null){
         knex.select('projectid','project_name','project_text','project_karma','project_projecttype','project_author','project_activity','project_statement','project_imagepath','project_writeRights','project_membercount','project_favcount','project_termin','project_change','Project_projectid','project_created_at','project_updated_at')
         .from('Project')
         .where('project_projecttype', "newProject")
-        .andWhere('project_statement', req.body.project_statement)
+        .andWhere('project_activity', req.body.project_activity)
         .andWhere('project_name', 'like', '%' + req.params.project_name + '%')
         .union (function(){
             this.select('projectid','project_name','project_text','project_karma','project_projecttype','project_author','project_activity','project_statement','project_imagepath','project_writeRights','project_membercount','project_favcount','project_termin','project_change','Project_projectid','project_created_at','project_updated_at')
@@ -49,7 +49,7 @@ function searchString(req,res){
                 .join('Resource', 'resourceid', 'phr_idresource')
                 .where('project_projecttype', "newProject")
                 .andWhere('resource_name', 'like', '%' + req.params.project_name + '%')
-                .andWhere('project_statement', req.body.project_statement)
+                .andWhere('project_activity', req.body.project_activity)
         })
         .then( User => res.send(User))
 
